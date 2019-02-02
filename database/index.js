23230813a0639f5c10287495234f2fbb70c3f03b
Aimen Altaiyeb <ymnghost@gmail.com>
2019-02-02 11:06:11 -0800

    Complete reposSchema.

--- database/index.js
+++ database/index.js
@@ -1,13 +1,25 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
-let repoSchema = mongoose.Schema({
+var dataBase = mongoose.connection;
+
+dataBase.on('error', console.error.bind(console, 'connection error:'));
+dataBase.once('open', function () {
+  console.log('works!!!');
+});
+//added 'new' in the line below, 
+let repoSchema = new mongoose.Schema({
   // TODO: your schema here!
+  name: String,
+  creator: String,
+  repoId: Number,
+  dateUpdated: String,
+
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = (/* TODO */  ) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
